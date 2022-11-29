97b0b7d209ef97bb180e3af70b98593292d9c783
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2022-11-29 18:10:05 +0600

    admin

--- src/Pages/Dashboard/AllBuyers/AllBuyers.jsx
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.jsx
@@ -3,9 +3,40 @@ import { useLoaderData } from "react-router-dom";
 
 const AllBuyers = () => {
   const buyers = useLoaderData();
+
   return (
     <div>
-      <h1>all buyers{buyers.length}</h1>
+      <h1 className="mt-5 mb-5 mx-5 text-2xl font-semibold sm:text-4xl text-[#e9c46a]">
+        All Buyers
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full ">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>email</th>
+              <th>Admin</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {buyers.map((buyer, index) => (
+              <tr key={buyer._id}>
+                <th>{index + 1}</th>
+                <td>{buyer.name}</td>
+                <td>{buyer.email}</td>
+                <td>
+                  <button className="btn btn-sm bg-green-600">Admin</button>
+                </td>
+                <td>
+                  <button className="btn btn-sm bg-red-600">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.jsx
+++ src/Pages/Dashboard/AllSellers/AllSellers.jsx
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { useLoaderData } from "react-router-dom";
 
 const AllSellers = () => {
-  const Seller = useLoaderData();
+  const Sellers = useLoaderData();
+
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+      headers: {
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success(`admin created successfully ${Sellers.name}`);
+        }
+      });
+  };
   return (
     <div>
-      <h1>sellers{Seller?.length}</h1>
+      <h1 className="mt-5 mb-5 mx-5 text-2xl font-semibold sm:text-4xl text-[#e9c46a]">
+        all Sellers
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full ">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>email</th>
+              <th>Admin</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Sellers.map((seller, index) => (
+              <tr key={seller._id}>
+                <th>{index + 1}</th>
+                <td>{seller.name}</td>
+                <td>{seller.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleMakeAdmin(seller._id)}
+                    className="btn btn-sm bg-green-600"
+                  >
+                    Admin
+                  </button>
+                </td>
+                <td>
+                  <button className="btn btn-sm bg-red-600">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
